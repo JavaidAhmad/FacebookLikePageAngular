f3ffdd1b062df62662bef66e486682424804ec80
JavaidAhmad <33113461+JavaidAhmad@users.noreply.github.com>
2018-02-07 10:01:56 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,12 @@ declare var userID;
 })
 
 export class AppComponent implements OnInit {
-  appId:string ='893613067486252'
-  pageId:string = '1742888795932825'
+  appId:string ='Your facebook app id'
+  pageId:string = 'Your Facebook Page Id'
   flag:Boolean=false;
   access_Tokenn:string;
   configUrl:string;
-  myPageName:string= "Newtowordpress";
+  myPageName:string= "PageNAme";
   Url = "https://graph.facebook.com/v2.12/me?fields=address%2Cid%2Clikes.limit(1000)&access_token=";
   constructor(private facebookService:FacebookInitService,private http:HttpClient,private _changeDetectorRef:ChangeDetectorRef){}
   ngOnInit(){
